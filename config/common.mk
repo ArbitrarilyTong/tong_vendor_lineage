88c8e2a02382b1a33cdbc04e9e08a16f88775cf6
RealLanta <lanta@bangumi.cyou>
2023-07-06 00:24:47 +0800

    vendor: Welcome to ArbitrarilyTong 2.0

--- config/common.mk
+++ config/common.mk
@@ -1,9 +1,12 @@
 # Allow vendor/extra to override any property by setting it first
 $(call inherit-product-if-exists, vendor/extra/product.mk)
 
-PRODUCT_BRAND ?= LineageOS
+PRODUCT_BRAND ?= ArbitrarilyTong
 
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
+
+# GAPPS
+$(call inherit-product, vendor/gapps/common.mk)
 
 ifeq ($(PRODUCT_GMS_CLIENTID_BASE),)
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
