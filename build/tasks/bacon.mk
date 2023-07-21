83a7a43729c5fc9425fb0317f93dc49a219a05c0
sadoyui <wiliamweiyi@gmail.com>
2023-07-21 21:20:02 +0000

    vendor: Go to LitmusOS V1.0

--- build/tasks/bacon.mk
+++ build/tasks/bacon.mk
@@ -16,7 +16,7 @@
 # -----------------------------------------------------------------
 # Lineage OTA update package
 
-LINEAGE_TARGET_PACKAGE := $(PRODUCT_OUT)/ArbitrarilyTong-$(LINEAGE_VERSION).zip
+LINEAGE_TARGET_PACKAGE := $(PRODUCT_OUT)/LitmusOS-$(LINEAGE_VERSION).zip
 
 SHA256 := prebuilts/build-tools/path/$(HOST_PREBUILT_TAG)/sha256sum
 
